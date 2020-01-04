2b87001a78a5117c68f6906b873782f836ee0936
hung.pv <hphamviet83@gmail.com>
2020-01-04 13:16:07 +0700

    sua

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ from markov.M_M_C_K import M_M_C_K
 # markov4 = M_M_C_K(8, 10, 1, 3)
 # markov4.diplay()
 
+# markov5 = M_M_C(6, 8, 2)
+# markov5.diplay()
+# r = markov5.L()*8*30*200000.0 - markov5.C*7000000.0
+# print("Loi nhuan hang thang la: "+str(r))
 markov5 = M_M_1_K(6, 8, 4)
 markov5.diplay()
 r = markov5.L()*8*30*200000.0 - (markov5.K-4)*2000000 - 7000000
